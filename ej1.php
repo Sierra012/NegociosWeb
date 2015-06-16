b9fc85eb60de0a96c99349d893aaebfac4a5aba4
Estudiante <Sierra012>
2015-06-15 19:52:39 -0600

    Formulario Nombre

---
+++ ej1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>Formulario de HTML y PHP</title>
+</head>
+<body>
+  <form action="formulario.php" method="get">
+    <label for="txtNombre">Nombre</label>
+    <input type="text" id="txtNOmbre"
+    name="txtNombre" placeholder="Nombre Completo"
+    />
+    <br/>
+
+    <input type="submit" value="Procesar"
+    id="btnProcesar" name="btnProcesar" />
+    <br/>
+
+  </form>
+</body>
+</html>
